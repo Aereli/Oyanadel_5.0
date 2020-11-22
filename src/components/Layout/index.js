c41aaae22add022456c96269b6690e445defd7f4
Eric <eric.oyanadel@nbcuni.com>
2020-11-22 13:26:17 -0500

    fix build

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -8,7 +8,6 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
-import "../../assets/globals.scss"
 import "./styles.scss"
 
 import Header from "../Header"
