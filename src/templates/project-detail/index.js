278eeeaa2bf5635b62498815f7cadcf4b1b18b02
Eric <eric.oyanadel@nbcuni.com>
2020-11-22 12:58:39 -0500

    build and style project template with createPage nodes

--- src/templates/project-detail/index.js
+++ src/templates/project-detail/index.js
@@ -2,6 +2,9 @@ import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../../components/layout"
 import SEO from "../../components/seo"
+import Img from "gatsby-image"
+import VideoModal from "../../components/Gallery/VideoModal"
+import "./styles.scss"
   
 export const query = graphql`
   query ProjectDetailQuery($slug: String!) {
@@ -17,12 +20,7 @@ export const query = graphql`
       image {
         childImageSharp {
           fluid {
-            base64
-            tracedSVG
-            srcWebp
-            srcSetWebp
-            originalImg
-            originalName
+            ...GatsbyImageSharpFluid
           }
         }
       }
@@ -35,8 +33,26 @@ export const query = graphql`
   return (
     <Layout>
       <SEO title="Project" />
+      <div className="projectDetailContainer">
 
-      <h1>{project.title}</h1>
+        <hr />
+          <div className="detailImage">
+            <Img fluid={project.image.childImageSharp.fluid} alt={project.title}/>
+          </div>
+        <hr />
+
+        <h1>{project.title}</h1>
+
+        <ul>
+          <li><a href={project.url} target="_blank" rel="noreferrer" >View The Site</a></li>
+          <li><a href={project.github} target="_blank" rel="noreferrer" >View Github</a></li>
+          <VideoModal video={project.video} />
+        </ul>
+
+        <p>Tech Used: {project.tech}</p>
+        <p>{project.description}</p>
+
+      </div>
     </Layout>
   )
 }
