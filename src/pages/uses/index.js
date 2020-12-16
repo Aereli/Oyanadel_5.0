2ae75c819cc1c9ca3b0fe372a96a8b372bbc7fdc
Eric <aerelioyanadel@gmail.com>
2020-12-16 17:08:15 -0500

    create uses tech page

---
+++ src/pages/uses/index.js
@@ -0,0 +1,71 @@
+import { graphql } from "gatsby"
+import React from "react"
+import Layout from "../../components/Layout"
+import SEO from "../../components/seo"
+import Img from "gatsby-image"
+import styles from "./styles.module.scss"
+
+function Uses({ data }) {
+  const uses = data.allUsesJson.nodes[0].usesInfo
+
+  return (
+    <Layout>
+      <SEO title="Uses" />
+      <div className={styles.container}>
+        <div className={styles.header}>
+          <h1>Hi There! This page shows my setup and software that I use.</h1>
+          <h2>
+            WesBos inspired this page and showcased it on his site!!{" "}
+            <span role="img">👇</span>
+          </h2>
+          <a href="wesbos.com/uses" target="_blank" rel="noopener noreferrer">
+            wesbos.com
+          </a>
+        </div>
+        <Img fluid={data.file.childImageSharp.fluid} alt="computer desk" />
+
+        {uses.map(item => (
+          <div className={styles.content}>
+            <h2 key={item.id}>{item.title}</h2>
+            {item.content.map(content => (
+              <p>
+                <a href={content.url} target="_blank" rel="noopener noreferrer">
+                  {content.title}{" "}
+                </a>
+                {content.description}
+              </p>
+            ))}
+          </div>
+        ))}
+      </div>
+    </Layout>
+  )
+}
+
+export default Uses
+
+export const data = graphql`
+  query {
+    file(relativePath: { eq: "computer_desk.JPG" }) {
+      childImageSharp {
+        fluid(maxWidth: 1000, quality: 100) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    allUsesJson {
+      nodes {
+        usesInfo {
+          id
+          title
+          content {
+            id
+            title
+            url
+            description
+          }
+        }
+      }
+    }
+  }
+`
